fa7db8cb6b2cf8f9564ddbc49b0c3cb53cf900d4
Bernard Ojengwa <bernardojengwa@gmail.com>
2019-08-14 23:20:44 +0100

    initial commit

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Devporta Docs',
+  logo: '/assets/img/logo.png',
+  description: 'Devporta API documentation.',
+  themeConfig: {
+    sidebar: [
+      '/',
+      '/sdk'
+    ],
+    nav: [
+      { text: 'Getting Started', link: '/getting-started/' },
+      { text: 'API', link: '/api/' },
+    ]
+  }
+}
